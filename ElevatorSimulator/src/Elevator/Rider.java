2f913475d81bb8b303542180d20d4fa24e1eb159
yanchengzeng <yz203@duke.edu>
2015-01-05 14:59:21 -0500

    Update Rider.java

--- ElevatorSimulator/src/Elevator/Rider.java
+++ ElevatorSimulator/src/Elevator/Rider.java
@@ -21,34 +21,17 @@ public class Rider implements Runnable{
 	public void run() {
 		if (upOr){
 			do{
-			System.out.println("Call Up from passenger "+riderID+" from floor "+from);
-			
-			elevator = (Elevator) building.CallUp(from);
-			System.out.println("Passenger "+ riderID + " is going to enter the elevator "+elevator.elevatorId+" from floor "+from);
-			}while(!elevator.Enter());
-			//{
-			//	System.out.println("Thread "+Thread.currentThread().getId()+" Keeps Trying.");
-			//}
-			//elevator.Enter();
-			System.out.println("Passenger 1" + riderID + " successfully enters the elevator "+elevator.elevatorId);
+				elevator = (Elevator) building.CallUp(from);
+			} while(!elevator.Enter());
 			elevator.RequestFloor(to);
-			System.out.println("Passenger 1" + riderID + " needs to go to floor "+ to);
 			elevator.Exit();
-			System.out.println("Passenger 1" + riderID + " successfully arrives");
 		}
 		else {
 			do{
-				System.out.println("Call Down from passenger "+riderID+" from floor "+from);
-				
 				elevator = (Elevator) building.CallDown(from);
-				System.out.println("Passenger "+ riderID + " is going to enter the elevator "+elevator.elevatorId+" from floor "+from);
-				}while(!elevator.Enter());
-			
-			System.out.println("Passenger 1" + riderID + " successfully enters the elevator");
+			} while(!elevator.Enter());
 			elevator.RequestFloor(to);
-			System.out.println("Passenger 1" + riderID + "  needs to go to floor "+ to);
 			elevator.Exit();
-			System.out.println("Passenger 1" + riderID + " successfully arrives on floor" + to);
 		}
 	}	
 }
