7df0ae8c3bcf3b353bc453aae8a23a340946a346
yanchengzeng <yz203@duke.edu>
2015-01-05 15:00:19 -0500

    Update AbstractElevator.java

--- ElevatorSimulator/src/Elevator/AbstractElevator.java
+++ ElevatorSimulator/src/Elevator/AbstractElevator.java
@@ -6,45 +6,21 @@ public abstract class AbstractElevator {
 	protected int elevatorId;
 	protected int maxOccupancyThreshold;
 
-	/**
-	 * Other variables/data structures as needed goes here 
-	 */
-
 	public AbstractElevator(int numFloors, int elevatorId, int maxOccupancyThreshold) {
 		this.numFloors = numFloors;
 		this.elevatorId = elevatorId;
 		this.maxOccupancyThreshold = maxOccupancyThreshold;
 	}
 
-	/**
-	 * Elevator control interface: invoked by Elevator thread.
- 	 */
-
-	/* Signal incoming and outgoing riders */
 	public abstract void OpenDoors(); 	
 
-	/**
-	 * When capacity is reached or the outgoing riders are exited and
-	 * incoming riders are in. 
- 	 */
 	public abstract void ClosedDoors();
 
-	/* Go to a requested floor */
 	public abstract void VisitFloor(int floor);
 
-
-	/**
-	 * Elevator rider interface (part 1): invoked by rider threads. 
-  	 */
-
-	/* Enter the elevator */
 	public abstract boolean Enter();
 	
-	/* Exit the elevator */
 	public abstract void Exit();
 
-	/* Request a destination floor once you enter */
  	public abstract void RequestFloor(int floor);	
-	
-	/* Other methods as needed goes here */
 }
